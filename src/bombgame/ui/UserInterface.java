599857eaaa8a43b8cfd4b2ed7e01dcec88a051e9
Rookfighter <456andy@gmx.de>
2012-11-23 12:03:53 +0100

    added UserInterface

---
+++ src/bombgame/ui/UserInterface.java
@@ -0,0 +1,9 @@
+package bombgame.ui;
+
+public interface UserInterface {
+	/**
+	 * Updates everything in the UserInterface.
+	 * @return - true if game should continue
+	 */
+	public boolean update();
+}
