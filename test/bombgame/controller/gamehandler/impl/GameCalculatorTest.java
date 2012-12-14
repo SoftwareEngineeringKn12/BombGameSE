acca5bcbfff6869aba392759bf360359d3a3b707
Fabian <Fabian@Rook-Netbook>
2012-12-14 17:15:45 +0100

    added some tests

--- test/bombgame/controller/gamehandler/impl/GameCalculatorTest.java
+++ test/bombgame/controller/gamehandler/impl/GameCalculatorTest.java
@@ -2,14 +2,28 @@ package bombgame.controller.gamehandler.impl;
 
 import java.util.ArrayList;
 
+import bombgame.entities.IExplosion;
+import bombgame.entities.impl.Bomb;
 import bombgame.entities.impl.Explosion;
+import bombgame.entities.impl.GameObject;
 import bombgame.entities.impl.Man;
 import bombgame.entities.impl.Wall;
 import junit.framework.TestCase;
 
 public final class GameCalculatorTest extends TestCase {
 
+	private GameCalculator gc1;
+	private GameHandler gh1;
+	private GameCalculator gc2;
+	private GameHandler gh2;
+	
 	public void setUp() {
+		gh1 = new GameHandler(new GameObject[3][3]);
+		gc1 = new GameCalculator(gh1);
+		
+		gh2 = new GameHandler(new GameObject[10][10]);
+		gc2 = new GameCalculator(gh2);
+		
 		
 	}
 	
@@ -17,141 +31,151 @@ public final class GameCalculatorTest extends TestCase {
 		
 		Man man = new Man(0,1);
 		
-		gh2.addObject(man);
-		gh2.addObject(new Wall(0,0));
-		gh2.addObject(new Wall(0,2));
+		gh1.addObject(man);
+		gh1.addObject(new Wall(0,0));
+		gh1.addObject(new Wall(0,2));
 		
-		gh2.moveMan(man);
+		gc1.moveMan(man);
 		assertEquals(man.getX(), 0);
 		assertEquals(man.getY(), 1);
 		
 		man.setDirection(Man.NO_DIR);
-		gh2.moveMan(man);
+		gc1.moveMan(man);
 		assertEquals(man.getX(), 0);
 		assertEquals(man.getY(), 1);
 		
 		man.setDirection(Man.UP);
-		gh2.moveMan(man);
+		gc1.moveMan(man);
 		assertEquals(man.getX(), 0);
 		assertEquals(man.getY(), 1);
 		
 		man.setDirection(Man.LEFT);
-		gh2.moveMan(man);
+		gc1.moveMan(man);
 		assertEquals(man.getX(), 0);
 		assertEquals(man.getY(), 1);
 		
 		man.setDirection(Man.DOWN);
-		gh2.moveMan(man);
+		gc1.moveMan(man);
 		assertEquals(man.getX(), 0);
 		assertEquals(man.getY(), 1);
 		
 		man.setDirection(Man.RIGHT);
-		gh2.moveMan(man);
+		gc1.moveMan(man);
 		assertEquals(man.getX(), 1);
 		assertEquals(man.getY(), 1);
 		
 		man.setDirection(Man.UP);
-		gh2.moveMan(man);
+		gc1.moveMan(man);
 		assertEquals(man.getX(), 1);
 		assertEquals(man.getY(), 0);
 		
 		man.setDirection(Man.UP);
-		gh2.moveMan(man);
+		gc1.moveMan(man);
 		assertEquals(man.getX(), 1);
 		assertEquals(man.getY(), 0);
 		
 		man.setDirection(Man.DOWN);
-		gh2.moveMan(man);
+		gc1.moveMan(man);
 		assertEquals(man.getX(), 1);
 		assertEquals(man.getY(), 1);
 		
 		man.setDirection(Man.DOWN);
-		gh2.moveMan(man);
+		gc1.moveMan(man);
 		assertEquals(man.getX(), 1);
 		assertEquals(man.getY(), 2);
 		
 		man.setDirection(Man.DOWN);
-		gh2.moveMan(man);
+		gc1.moveMan(man);
 		assertEquals(man.getX(), 1);
 		assertEquals(man.getY(), 2);
 		
 		man.setDirection(Man.UP);
-		gh2.moveMan(man);
+		gc1.moveMan(man);
 		assertEquals(man.getX(), 1);
 		assertEquals(man.getY(), 1);
 		
 		man.setDirection(Man.RIGHT);
-		gh2.moveMan(man);
+		gc1.moveMan(man);
+		assertEquals(man.getX(), 2);
+		assertEquals(man.getY(), 1);
+		
+		man.setDirection(Man.RIGHT);
+		gc1.moveMan(man);
+		assertEquals(man.getX(), 2);
+		assertEquals(man.getY(), 1);
+		
+		man.setDirection(Man.LEFT);
+		gc1.moveMan(man);
 		assertEquals(man.getX(), 1);
 		assertEquals(man.getY(), 1);
 		
 		Wall w = new Wall(0,1);
-		gh2.addObject(w);
+		gh1.addObject(w);
 		
 		man.setDirection(Man.LEFT);
-		gh2.moveMan(man);
+		gc1.moveMan(man);
 		assertEquals(man.getX(), 1);
 		assertEquals(man.getY(), 1);
 		
-		gh2.removeObject(w);
+		gh1.removeObject(w);
 		
 		man.setDirection(Man.LEFT);
-		gh2.moveMan(man);
+		gc1.moveMan(man);
 		assertEquals(man.getX(), 0);
 		assertEquals(man.getY(), 1);
 		
-		gh2.addObject(new Wall(1,1));
+		gh1.addObject(new Wall(1,1));
 		
 		man.setDirection(Man.RIGHT);
-		gh2.moveMan(man);
+		gc1.moveMan(man);
 		assertEquals(man.getX(), 0);
 		assertEquals(man.getY(), 1);
 		
 		man.setDirection(-1);
-		gh2.moveMan(man);
+		gc1.moveMan(man);
 		assertEquals(man.getX(), 0);
 		assertEquals(man.getY(), 1);
 		
 		
 		//#############
 		man = new Man(1,1);
-		gh1.addObject(man);
-		gh1.addObject(new Man(0,1));
-		gh1.addObject(new Man(1,0));
-		gh1.addObject(new Man(1,2));
-		gh1.addObject(new Man(2,1));
+		gh2.addObject(man);
+		gh2.addObject(new Man(0,1));
+		gh2.addObject(new Man(1,0));
+		gh2.addObject(new Man(1,2));
+		gh2.addObject(new Man(2,1));
 		
 		
 		man.setDirection(Man.LEFT);
-		gh1.moveMan(man);
+		gc2.moveMan(man);
 		assertEquals(man.getX(), 0);
 		assertEquals(man.getY(), 1);
 		
 		man.setDirection(Man.UP);
-		gh1.moveMan(man);
+		gc2.moveMan(man);
 		man.setDirection(Man.RIGHT);
-		gh1.moveMan(man);
+		gc2.moveMan(man);
 		assertEquals(man.getX(), 1);
 		assertEquals(man.getY(), 0);
 		
 		man.setDirection(Man.RIGHT);
-		gh1.moveMan(man);
+		gc2.moveMan(man);
 		man.setDirection(Man.DOWN);
-		gh1.moveMan(man);
+		gc2.moveMan(man);
 		assertEquals(man.getX(), 2);
 		assertEquals(man.getY(), 1);
 		
 		man.setDirection(Man.DOWN);
-		gh1.moveMan(man);
+		gc2.moveMan(man);
 		man.setDirection(Man.LEFT);
-		gh1.moveMan(man);
+		gc2.moveMan(man);
 		assertEquals(man.getX(), 1);
 		assertEquals(man.getY(), 2);
 		
 		man.setDirection(Man.LEFT);
-		gh1.moveMan(man);
+		gc2.moveMan(man);
 		man.setDirection(Man.UP);
-		gh1.moveMan(man);
+		gc2.moveMan(man);
 		assertEquals(man.getX(), 0);
 		assertEquals(man.getY(), 1);
 	}
@@ -159,12 +183,35 @@ public final class GameCalculatorTest extends TestCase {
 	// Calc expl
 	
 	public void testNextExplosion() {
-		gh1.addObject(new Wall(3,4));
-		ArrayList<Explosion> al = new ArrayList<Explosion>();
-		assertFalse(gh1.nextExplosion(-1, 0, true, al));
-		assertFalse(gh1.nextExplosion(15, 0, true, al));
-		assertFalse(gh1.nextExplosion(-0, -1, true, al));
-		assertFalse(gh1.nextExplosion(0, 15, true, al));
+		gh2.addObject(new Wall(3,4));
+		ArrayList<IExplosion> al = new ArrayList<IExplosion>();
+		assertFalse(gc2.nextExplosion(-1, 0, true, al));
+		assertFalse(gc2.nextExplosion(15, 0, true, al));
+		assertFalse(gc2.nextExplosion(-0, -1, true, al));
+		assertFalse(gc2.nextExplosion(0, 15, true, al));
+		
+		assertTrue(gc2.nextExplosion(0,0,true,al));
+		assertTrue(gc2.nextExplosion(0,9,true,al));
+		assertTrue(gc2.nextExplosion(9,0,true,al));
+		assertTrue(gc2.nextExplosion(9,9,true,al));
+		
+		assertFalse(gc2.nextExplosion(0,0,false,al));
+		
+		gh2.addObject(new Wall(1,1));
+		assertFalse(gc2.nextExplosion(1,1,true,al));
+		
+		gh2.addObject(new Bomb(2,2));
+		assertTrue(gc2.nextExplosion(2,2,true,al));
+	}
+	
+	public void testCalculateExplosion(){
+		
+		assertNotNull(gc2.calculateExplosion(new Explosion(2,2)));
+		
+	}
+	
+	public void testInitializeField() {
+		gc1.initializeField(4, 4);
 	}
 	
 }
--- test/bombgame/controller/gamehandler/impl/GameToStringTest.java
+++ test/bombgame/controller/gamehandler/impl/GameToStringTest.java
@@ -1,12 +1,21 @@
 package bombgame.controller.gamehandler.impl;
 
+import bombgame.controller.PlayerTUI;
+import bombgame.controller.ai.ManAI;
+import bombgame.entities.impl.Bomb;
 import bombgame.entities.impl.Explosion;
+import bombgame.entities.impl.GameObject;
+import bombgame.entities.impl.Man;
 import junit.framework.TestCase;
 
 public final class GameToStringTest extends TestCase {
 
+	private GameHandler gh1;
+	private GameToString gts;
+	
 	public void setUp() {
-		
+		gh1 = new GameHandler(new GameObject[10][10]);
+		gts = new GameToString(gh1);
 	}
 	
 	public void testExplosionToString() {
@@ -15,6 +24,30 @@ public final class GameToStringTest extends TestCase {
 		String s1 = gh1.explosionListToString();
 		String s2 = "->Explosion: { [0] [0], [1] [0], [0] [1], [2] [0], [0] [2], [3] [0], [0] [3] }\n->Explosion: { [9] [9], [8] [9], [9] [8], [7] [9], [9] [7], [6] [9], [9] [6] }\n";
 		assertEquals(s1,s2);
+	}
+	
+	public void testToString() {
+		gh1.addObject(new Explosion(0,0));
+		gh1.addObject(new Explosion(9,9));
+		
+		Man man = new Man(2,2);
+		gh1.addObject(man);
+		PlayerTUI player = new PlayerTUI(man);
+		gh1.setPlayer(player);
+		
+		Man man2 = new Man(3,3);
+		gh1.addObject(man2);
+		ManAI ai = new ManAI(man2,gh1);
+		gh1.addAI(ai);
+		
+		gh1.addObject(new Bomb(4,3));
+		
+		StringBuilder sb = new StringBuilder("Players:\n");
+		sb.append("-> Player: [2] [2] Direction: ").append(Man.NO_DIR).append("\n");
+		sb.append("AI:\n");
+		sb.append("Bombs:\n");
+		sb.append("Explosions:\n->Explosion: { [0] [0], [1] [0], [0] [1], [2] [0], [0] [2], [3] [0], [0] [3] }\n->Explosion: { [9] [9], [8] [9], [9] [8], [7] [9], [9] [7], [6] [9], [9] [6] }\n");
+		gts.toString();
 	}
 	
 }
