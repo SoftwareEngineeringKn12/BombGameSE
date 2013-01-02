f865abc5f90518d7932d920fdbb4662e81f5211f
jens <jens@silver-jens>
2013-01-02 16:22:25 +0100

    wrote tests

--- src/bombgame/application/App.java
+++ src/bombgame/application/App.java
@@ -12,28 +12,50 @@ import bombgame.ui.UserInterface;
  *
  */
 public final class App {
+	
+	/**
+	 * Scanner for TUI
+	 */
+	private Scanner in;
+	
+	/**
+	 * TUI
+	 */
+	private UserInterface tui;
+	
+	/**
+	 * Configures log4j, creates Scanner and TUI.
+	 */
+	public App() {
+		// Configure log4j
+		PropertyConfigurator.configureAndWatch("log4j.properties");
+		
+		// Scanner for TUI
+		in = new Scanner(System.in);
+		
+		// Create TUI
+		GameHandlerConfiguration config = new GameHandlerConfiguration(30, 20, 3);
+		tui = new TextUI(config.createGameHandler());
+	}
+	
+	/**
+	 * Starts the TUI loop
+	 */
+	public void start() {
+		// LOOP
+		boolean cont = true;
+		while(cont) {
+			cont = tui.update(in.next());
+		}
+		in.close();
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// Configure log4j
-		PropertyConfigurator.configureAndWatch("log4j.properties");
-		
-		// Scanner for TUI
-		Scanner in = new Scanner(System.in);
-		
-		// Create TUI - gets concrete Player and AI
-		// Here than player, man, mazegen -> googleguice
-		GameHandlerConfiguration config = new GameHandlerConfiguration(30, 20, 3);
-		UserInterface ui = new TextUI(config.createGameHandler());
-		
-		// LOOP
-		boolean cont = true;
-		while(cont) {
-			cont = ui.update(in.next());
-		}
-		in.close();
+		App application = new App();
+		application.start();
 	}
 
 }
